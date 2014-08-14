1de06db602da3d6a3cc6852b698bd416838430a1
DenisKuzovin <kuzovin@gmail.com>
2014-08-14 23:42:56 +0400

    Add script for build necessary libs

---
+++ library/build_libs_for_ad9361.tcl
@@ -0,0 +1,34 @@
+cd util_i2c_mixer
+source util_i2c_mixer_ip.tcl
+cd ..
+
+cd util_dac_unpack
+source util_dac_unpack_ip.tcl
+cd ..
+
+cd util_adc_pack
+source util_adc_pack_ip.tcl
+cd ..
+
+cd axi_spdif_tx
+source axi_spdif_tx_ip.tcl
+cd ..
+
+cd axi_i2s_adi
+source axi_i2s_adi_ip.tcl
+cd ..
+
+cd axi_hdmi_tx
+source axi_hdmi_tx_ip.tcl
+cd ..
+
+cd axi_dmac
+source axi_dmac_ip.tcl
+cd ..
+
+cd axi_clkgen
+source axi_clkgen_ip.tcl
+cd ..
+
+cd axi_ad9361
+source axi_ad9361_ip.tcl
